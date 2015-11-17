73283c3f3ac32c2064bf443a9c17069b39d70b27
aocbn <ledinhan.fit@gmail.com>
2015-11-17 17:41:58 +0700

    change package

--- app/src/main/java/com/alanddev/manwal/ui/ManWalActivity.java
+++ app/src/main/java/com/alanddev/manwal/ui/ManWalActivity.java
@@ -1,4 +1,4 @@
-package com.alanddev.manwal;
+package com.alanddev.manwal.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.alanddev.manwal.R;
 
 public class ManWalActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
